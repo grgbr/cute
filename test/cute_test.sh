d8655dbb2fb87a6036873ddbad14164b3799ac0c
Gregor Boirie <gregor.boirie@parrot.com>
2017-07-18 18:32:36 +0200

    shared library support

--- test/cute_test.sh
+++ test/cute_test.sh
@@ -5,6 +5,10 @@ builddir=$(realpath $srcdir/../build)
 
 for t in $srcdir/cute_test_*.c; do
 	base=$(basename $t)
-	echo "======== ${base%%.c} ========"
-	$builddir/${base%%.c}
+	echo "======== static ${base%%.c} ====================================="
+	$builddir/static/${base%%.c}
+	echo
+	echo "======== shared ${base%%.c} ====================================="
+	env LD_LIBRARY_PATH=$builddir $builddir/shared/${base%%.c}
+	echo
 done
