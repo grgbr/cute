20cff4f71346fa5d5c8544a25d46210391b2e9b6
Gregor Boirie <gregor.boirie@parrot.com>
2017-07-18 18:29:49 +0200

    ensure proper test execution ordering

--- include/cute/utils.h
+++ include/cute/utils.h
@@ -7,10 +7,21 @@
 #define __section(...) __attribute__((section(__VA_ARGS__)))
 #define __aligned(...) __attribute__((aligned(__VA_ARGS__)))
 
+/*
+ * Prevent reordering of top-level statements for optimization purposes (see
+ * -fno-toplevel-reorder gcc option.
+ */
+#define __noreorder    __attribute__((no_reorder))
+
 #define _CUTE_STR(_symbol) # _symbol
 #define CUTE_STR(_symbol)  _CUTE_STR(_symbol)
 
+/*
+ * __noreorder is needed to ensure gcc optimization preserves order in
+ * which symbols were defined in source file, providing test writer with
+ * full control over tests execution order.
+ */
 #define __cute_pnp(...) \
-	__section(__VA_ARGS__) __aligned(__SIZEOF_POINTER__) __used
+	__section(__VA_ARGS__) __aligned(__SIZEOF_POINTER__) __used __noreorder
 
 #endif
