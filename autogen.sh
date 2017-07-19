1b7568adf7c0b0531d5bbd2f0a457bdc3e268c63
Gregor Boirie <gregor.boirie@parrot.com>
2017-07-19 17:04:43 +0200

    amend me

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-autoreconf -if
+# Autoreconf will produce necessary files needed to run configure.
+# It will complain (and fail) since we use no Makefile.am setup...
+autoreconf --install --force >/dev/null 2>&1 || :;
+
+# Generate autoconf macros into aclocal.m4, otherwise they will not be seen
+# by autoconf
+#aclocal --force
+
+# Generate configure script using macros defined above
+#autoconf
+
+# Generate config.h.in
+#autoheader --force
+
+# Generate auxiliary files
+#mkdir build-aux
+#automake --add-missing --copy --force-missing --foreign >/dev/null 2>&1 || :;
