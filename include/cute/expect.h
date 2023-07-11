7528d627a81488ffd1367b354a432b6dcd7577f7
Gregor Boirie <gregor.boirie@free.fr>
2023-07-11 19:41:05 +0200

    initial floating point mocking implementation

--- include/cute/expect.h
+++ include/cute/expect.h
@@ -26,12 +26,11 @@ cute_expect_check_call(const char * file, int line, const char * function)
  ******************************************************************************/
 
 #define cute_expect_sint_parm(_func, _parm, _op, _xpct) \
-	cute_expect_sched_sint_parm_ ##  _op( \
-		__FILE__, \
-		__LINE__, \
-	        # _func, \
-	        # _parm, \
-		&__CUTE_VALUE(sint, _xpct))
+	cute_expect_sched_sint_parm_ ##  _op(__FILE__, \
+	                                     __LINE__, \
+	                                     # _func, \
+	                                     # _parm, \
+		                             &__CUTE_VALUE(sint, _xpct))
 
 #define cute_mock_sint_parm(_parm) \
 	cute_expect_check_sint_parm(__FILE__, \
@@ -172,12 +171,11 @@ cute_expect_check_sint_retval(const char * file,
  ******************************************************************************/
 
 #define cute_expect_uint_parm(_func, _parm, _op, _xpct) \
-	cute_expect_sched_uint_parm_ ##  _op( \
-		__FILE__, \
-		__LINE__, \
-	        # _func, \
-	        # _parm, \
-		&__CUTE_VALUE(uint, _xpct))
+	cute_expect_sched_uint_parm_ ##  _op(__FILE__, \
+	                                     __LINE__, \
+	                                     # _func, \
+	                                     # _parm, \
+	                                     &__CUTE_VALUE(uint, _xpct))
 
 #define cute_mock_uint_parm(_parm) \
 	cute_expect_check_uint_parm(__FILE__, \
@@ -312,5 +310,150 @@ cute_expect_check_uint_retval(const char * file,
 
 #define cute_mock_uint_retval() \
 	cute_expect_check_uint_retval(__FILE__, __LINE__, __func__)
+
+/******************************************************************************
+ * Floating point number mock parameter expectation handling
+ ******************************************************************************/
+
+#define cute_expect_flt_parm(_func, _parm, _op, _xpct) \
+	cute_expect_sched_flt_parm_ ##  _op(__FILE__, \
+	                                    __LINE__, \
+	                                    # _func, \
+	                                    # _parm, \
+	                                    &__CUTE_VALUE(flt, _xpct))
+
+#define cute_mock_flt_parm(_parm) \
+	cute_expect_check_flt_parm(__FILE__, \
+	                           __LINE__, \
+	                           __func__, \
+	                           &__CUTE_VALUE(flt, _parm))
+
+extern void
+cute_expect_check_flt_parm(const char *            file,
+                           int                     line,
+                           const char *            function,
+                           const struct cute_flt * check)
+	__cute_export;
+
+extern void
+cute_expect_sched_flt_parm_equal(const char *            file,
+                                 int                     line,
+                                 const char *            function,
+                                 const char *            parm,
+                                 const struct cute_flt * expect)
+	__cute_export;
+
+extern void
+cute_expect_sched_flt_parm_unequal(const char *            file,
+                                   int                     line,
+                                   const char *            function,
+                                   const char *            parm,
+                                   const struct cute_flt * expect)
+	__cute_export;
+
+extern void
+cute_expect_sched_flt_parm_greater(const char *            file,
+                                   int                     line,
+                                   const char *            function,
+                                   const char *            parm,
+                                   const struct cute_flt * expect)
+	__cute_export;
+
+extern void
+cute_expect_sched_flt_parm_greater_equal(const char *            file,
+                                         int                     line,
+                                         const char *            function,
+                                         const char *            parm,
+                                         const struct cute_flt * expect)
+	__cute_export;
+
+extern void
+cute_expect_sched_flt_parm_lower(const char *            file,
+                                 int                     line,
+                                 const char *            function,
+                                 const char *            parm,
+                                 const struct cute_flt * expect)
+	__cute_export;
+
+extern void
+cute_expect_sched_flt_parm_lower_equal(const char *            file,
+                                       int                     line,
+                                       const char *            function,
+                                       const char *            parm,
+                                       const struct cute_flt * expect)
+	__cute_export;
+
+#define cute_expect_flt_range(_func, _parm, _op, _xpct) \
+	cute_expect_sched_flt_parm_ ##  _op ## _range(__FILE__, \
+	                                              __LINE__, \
+	                                              # _func, \
+	                                              # _parm, \
+	                                              &(_xpct))
+
+extern void
+cute_expect_sched_flt_parm_in_range(const char *                  file,
+                                    int                           line,
+                                    const char *                  function,
+                                    const char *                  parm,
+                                    const struct cute_flt_range * expect)
+	__cute_export;
+
+extern void
+cute_expect_sched_flt_parm_not_in_range(
+	const char *                  file,
+	int                           line,
+	const char *                  function,
+	const char *                  parm,
+	const struct cute_flt_range * expect)
+	__cute_export;
+
+#define cute_expect_flt_set(_func, _parm, _op, _xpct) \
+	cute_expect_sched_flt_parm_ ##  _op ## _set(__FILE__, \
+	                                            __LINE__, \
+	                                            # _func, \
+	                                            # _parm, \
+	                                            &(_xpct))
+
+extern void
+cute_expect_sched_flt_parm_in_set(const char *                file,
+                                  int                         line,
+                                  const char *                function,
+                                  const char *                parm,
+                                  const struct cute_flt_set * expect)
+	__cute_export;
+
+extern void
+cute_expect_sched_flt_parm_not_in_set(const char *                file,
+                                      int                         line,
+                                      const char *                function,
+                                      const char *                parm,
+                                      const struct cute_flt_set * expect)
+	__cute_export;
+
+/******************************************************************************
+ * Floating point number return value expectation handling
+ ******************************************************************************/
+
+extern void
+cute_expect_sched_flt_retval(const char *            file,
+                             int                     line,
+                             const char *            function,
+                             const struct cute_flt * retval)
+	__cute_export;
+
+#define cute_expect_flt_retval(_func, _retval) \
+	cute_expect_sched_flt_retval(__FILE__, \
+	                             __LINE__, \
+	                             # _func, \
+	                             &__CUTE_VALUE(flt, _retval))
+
+extern long double
+cute_expect_check_flt_retval(const char * file,
+                             int          line,
+                             const char * function)
+	__cute_export;
+
+#define cute_mock_flt_retval() \
+	cute_expect_check_flt_retval(__FILE__, __LINE__, __func__)
 
 #endif /* _CUTE_EXPECT_H */
